c78ab8b8d545d93ff273926ded57621941ea187c
BeautifulBabe-MTM <yapitnichyk@gmail.com>
2024-03-01 18:19:06 +0200

    navbar added

--- src/App.js
+++ src/App.js
@@ -1,23 +1,53 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [activeTab, setActiveTab] = useState('Театр');
+
+  const handleTabClick = (tabName) => {
+    setActiveTab(tabName);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <nav className="navbar">
+        <div className="navbar-left">
+          <h1 className="madimi-one-regular">Art Neva</h1>
+        </div>
+        <div className="navbar-right">
+
+          <div
+            className={activeTab === 'Театр' ? 'tab active' : 'tab'}
+            onClick={() => handleTabClick('Театр')}
+          >
+            <h3 className='playfair-display-nav'>Театр</h3>
+          </div>
+          <div
+            className={activeTab === 'Опера' ? 'tab active' : 'tab'}
+            onClick={() => handleTabClick('Опера')}
+          >
+            <h3 className='playfair-display-nav'>Опера</h3>
+          </div>
+          <div
+            className={activeTab === 'Балет' ? 'tab active' : 'tab'}
+            onClick={() => handleTabClick('Балет')}
+          >
+            <h3 className='playfair-display-nav'>Балет</h3>
+          </div>
+          <div
+            className={activeTab === 'Стендап' ? 'tab active' : 'tab'}
+            onClick={() => handleTabClick('Стендап')}
+          >
+            <h3 className='playfair-display-nav'>Стендап</h3>
+          </div>
+          <div
+            className={activeTab === 'Выставки' ? 'tab active' : 'tab'}
+            onClick={() => handleTabClick('Выставки')}
+          >
+            <h3 className='playfair-display-nav'>Выставки</h3>
+          </div>
+        </div>
+      </nav>
     </div>
   );
 }
